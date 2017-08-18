82330aabe7d05e938b5ca79ba1382e18edadd204
minubalasubramaniam <minub@nmsworks.co.in>
2017-08-18 17:01:07 +0530

    index

---
+++ BeanInheritance/src/Second.java
@@ -0,0 +1,29 @@
+public class Second
+
+{
+	 private String message1;
+	 private String message2;
+	 private String message3;
+
+	   public void setMessage1(String message){
+	      this.message1 = message;
+	   }
+	   public void setMessage2(String message){
+	      this.message2 = message;
+	   }
+	   public void setMessage3(String message){
+	      this.message3 = message;
+	   }
+   public void display1()
+   {
+	   System.out.println(message1);
+   }
+   public void display2()
+   {
+	   System.out.println(message2);
+   }
+   public void display3()
+   {
+	   System.out.println(message3);
+   }
+}
